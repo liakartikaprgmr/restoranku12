8e24e590fa3543a6947d8c37baf5d4223cc8efda
liakartikaprgmr <liakartikanw@gmail.com>
2025-08-12 20:50:12 +0700

    `Added SoftDeletes trait, updated fillable attributes, added date attribute, and created relationships for User model`

---
+++ database/migrations/2025_08_05_211442_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_code')->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->integer('subtotal');
+            $table->integer('tax');
+            $table->integer('grand_total');
+            $table->enum('status', ['pending', 'settlement', 'cooked']);
+            $table->integer('table_number');
+            $table->enum('payment_method', ['tunai', 'qris']);
+            $table->text('note')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
