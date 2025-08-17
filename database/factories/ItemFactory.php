fe859f6da43e0a6a4f2999932c6f9d59c274b7bc
liakartikaprgmr <liakartikanw@gmail.com>
2025-08-17 14:59:30 +0700

    Updated ItemFactory to use predefined image URLs and modified DatabaseSeeder to call ItemSeeder.

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -14,7 +14,11 @@ class ItemFactory extends Factory
             'category_id' => $this->faker->numberBetween(1, 2),
             'price' => $this->faker->randomFloat(2, 1000, 100000),
             'description' => $this->faker->text(),
-            'img' => $this->faker->imageUrl(),
+            'img' => fake()->randomElement(
+                ['https://plus.unsplash.com/premium_photo-1694547926001-f2151e4a476b', 
+                'https://plus.unsplash.com/premium_photo-1668146927669-f2edf6e86f6f', 
+                'https://images.unsplash.com/photo-1738681335816-8e0df0aa9824']
+            ),
             'is_active' => $this->faker->boolean(),
         ];
     }
